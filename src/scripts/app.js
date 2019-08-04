8b3a0467fd8d893db8f5845dfe16ca39e97d2e5d
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-08-04 23:44:24 +0200

    23 - merge

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,49 @@
-function openCity(cityName) {
-  var i;
-  var x = document.getElementsByClassName('city');
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
+// Ocena w Gwiazdkach
+
+// Referencje do html-u
+
+const allStars = document.querySelectorAll('.stars');
+
+// Nasłuchiwacze
+
+allStars.forEach(function (item) {
+  const stars = item.querySelectorAll('.star');
+  stars.forEach(function (el) {
+    el.addEventListener('click', rating);
+    el.addEventListener('mouseover', hoverEffect);
+    el.addEventListener('mouseout', hoverEffect);
+  });
+
+  // Funkcja zmiany gwiazdki na kliknięcie
+
+  function rating (event) {
+    const choice = event.target;
+    let active = false;
+    stars.forEach(function (star) {
+      if (active) {
+        star.classList.remove('active');
+        star.classList.remove('full');
+      } else if (!active) {
+        star.classList.add('active');
+      }
+      if (star === choice) {
+        active = true;
+      }
+    });
   }
-  document.getElementById(cityName).style.display = 'block';
-}
+
+  // Funkcja zmiany gwiazdki na Hover
+
+  function hoverEffect (event) {
+    const choice = event.target;
+    let active = false;
+    stars.forEach(function (star) {
+      if (!active) {
+        star.classList.toggle('hoverEffect');
+      }
+      if (star === choice) {
+        active = !active;
+      }
+    });
+  }
+});
