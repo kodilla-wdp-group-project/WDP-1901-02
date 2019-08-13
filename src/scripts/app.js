5ee10b58ab601c19125f7ab7edc769dcdce348ec
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-08-13 23:11:11 +0200

    finish indoor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,115 +1,91 @@
-// Ocena w Gwiazdkach
+console.log('hello');
 
-// Referencje do html-u
 
-const allStars = document.querySelectorAll('.stars');
 
-// Nasłuchiwacze
+let list1 = document.querySelector("#bed");
+ 
+let list2 = document.querySelector("#chair");
+ 
+let list3 = document.querySelector("#sofa");
 
-allStars.forEach(function(item) {
-  const stars = item.querySelectorAll('.star');
-  stars.forEach(function(el) {
-    el.addEventListener('click', rating);
-    el.addEventListener('mouseover', hoverEffect);
-    el.addEventListener('mouseout', hoverEffect);
-  });
-
-  // Funkcja zmiany gwiazdki na kliknięcie
-
-  function rating(event) {
-    const choice = event.target;
-    let active = false;
-    stars.forEach(function(star) {
-      if (active) {
-        star.classList.remove('active');
-        star.classList.remove('full');
-      } else if (!active) {
-        star.classList.add('active');
-      }
-      if (star === choice) {
-        active = true;
-      }
-    });
-  }
-
-  // Funkcja zmiany gwiazdki na Hover
-
-  function hoverEffect(event) {
-    const choice = event.target;
-    let active = false;
-    stars.forEach(function(star) {
-      if (!active) {
-        star.classList.toggle('hoverEffect');
-      }
-      if (star === choice) {
-        active = !active;
-      }
-    });
-  }
-});
-
-let list1 = document.querySelector('#bed');
-
-let list2 = document.querySelector('#chair');
-
-let list3 = document.querySelector('#sofa');
-
-let list4 = document.querySelector('#table');
-
-let list5 = document.querySelector('#dining');
-
-let lists = document.querySelectorAll('.list');
+let list4 = document.querySelector("#table");
+ 
+let list5 = document.querySelector("#dining");
+ 
+let lists = document.querySelectorAll(".list");
 
 let page = document.querySelectorAll('.page');
 
 let menu = document.querySelector('.menu');
 
 let first = document.querySelector('#first');
-let second = document.querySelector('#second');
+let second =  document.querySelector('#second');
 
-list1.addEventListener('click', function() {
-  updatePage(first);
-  updateActiveStatus(list1);
-});
 
-list2.addEventListener('click', function() {
-  updatePage(second);
-  updateActiveStatus(list2);
-});
+list1.addEventListener("click", function()
+  {
+  	
+  	updatePage(first);
+    updateActiveStatus(list1);
+  }
+);
 
-list3.addEventListener('click', function() {
-  updatePage(first);
-  updateActiveStatus(list3);
-});
+list2.addEventListener("click", function()
+  {
+  	
+  	updatePage(second);
+    updateActiveStatus(list2);
+  }
+);
 
-list4.addEventListener('click', function() {
-  updatePage(second);
-  updateActiveStatus(list4);
-});
+list3.addEventListener("click", function()
+  {
+  	
+  	updatePage(first);
+    updateActiveStatus(list3);
+  }
+);
 
-list5.addEventListener('click', function() {
-  updatePage(first);
-  updateActiveStatus(list5);
-});
+list4.addEventListener("click", function()
+  {
+  	
+  	updatePage(second);
+    updateActiveStatus(list4);
+  }
+);
 
-function updateActiveStatus(active_item) {
-  lists.forEach(function(list_item) {
-    if (list_item != active_item) {
-      list_item.classList.remove('active');
-      menu.classList.add('animation');
-    } else {
-      list_item.classList.add('active');
+list5.addEventListener("click", function()
+  {
+ 	
+ 	updatePage(first);
+    updateActiveStatus(list5);
+  }
+);
+ 
+function updateActiveStatus(active_item){
+  lists.forEach(function(list_item)
+  {
+    if(list_item != active_item){
+      list_item.classList.remove("active");
       menu.classList.add('animation');
     }
-  });
+    else{
+      list_item.classList.add("active");
+      menu.classList.add('animation');
+    }
+  })
 }
 
-function updatePage(active_item) {
-  page.forEach(function(list_item) {
-    if (list_item != active_item) {
-      list_item.classList.add('temp');
-    } else {
-      list_item.classList.remove('temp');
+function updatePage(active_item){
+  page.forEach(function(list_item)
+  {
+    if(list_item != active_item){
+      list_item.classList.add("temp");
+   
     }
-  });
+    else{
+      list_item.classList.remove("temp");
+ 
+    }
+  })
 }
