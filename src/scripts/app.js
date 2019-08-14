a8117d68e1fd903dd2a7ffdbfd2f8e7ea7e0a018
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-08-14 12:03:24 +0200

    fixed indoor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log('hello');
 
 
@@ -52,64 +51,14 @@ list4.addEventListener("click", function()
   	
   	updatePage(second);
     updateActiveStatus(list4);
-=======
-// Ocena w Gwiazdkach
-
-// Referencje do html-u
-
-const allStars = document.querySelectorAll('.stars');
-
-// Nasłuchiwacze
-
-allStars.forEach(function (item) {
-  const stars = item.querySelectorAll('.star');
-  stars.forEach(function (el) {
-    el.addEventListener('click', rating);
-    el.addEventListener('mouseover', hoverEffect);
-    el.addEventListener('mouseout', hoverEffect);
-  });
-
-  // Funkcja zmiany gwiazdki na kliknięcie
-
-  function rating (event) {
-    const choice = event.target;
-    let active = false;
-    stars.forEach(function (star) {
-      if (active) {
-        star.classList.remove('active');
-        star.classList.remove('full');
-      } else if (!active) {
-        star.classList.add('active');
-      }
-      if (star === choice) {
-        active = true;
-      }
-    });
->>>>>>> fix master
   }
 );
 
-<<<<<<< HEAD
 list5.addEventListener("click", function()
   {
  	
  	updatePage(first);
     updateActiveStatus(list5);
-=======
-  // Funkcja zmiany gwiazdki na Hover
-
-  function hoverEffect (event) {
-    const choice = event.target;
-    let active = false;
-    stars.forEach(function (star) {
-      if (!active) {
-        star.classList.toggle('hoverEffect');
-      }
-      if (star === choice) {
-        active = !active;
-      }
-    });
->>>>>>> fix master
   }
 );
  
